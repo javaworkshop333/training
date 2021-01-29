a0b6863acb1bc807e3176d3f81a48837d4057b2d
javaworkshop333 javaworkshop333@gmail.com <javaworkshop333>
2021-01-28 19:43:57 -0600

    variables

---
+++ src/FacebookCustomer.java
@@ -0,0 +1,21 @@
+/*
+ FacebookCustomer
+	has - name, age,address,education,profession
+	does
+		post
+		chat
+ */
+
+public class FacebookCustomer{
+	//no white space			
+	// $ _ allowed special characters 	anywhere	
+	// numbers not in the begining			
+	// camel case - first chracter of every word is uppercase			
+
+	
+	String name="john";
+	byte age=10;
+	String _address="123 street, city, state";
+	String education1="Masters";
+	String profession="java programmer";
+}
